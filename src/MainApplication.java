49426493f7d0817178beb96c42c2264f41f0f6f1
Septima777 <noppawan.kul@ku.th>
2018-11-26 00:14:34 +0700

    add fxml file

---
+++ src/MainApplication.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application{
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Scanner.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/ScannerController.java
@@ -0,0 +1,31 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class ScannerController {
+
+	@FXML
+    private Button localDeviceButton;
+
+    @FXML
+    private Button ConnectedButton;
+
+    @FXML
+    private AnchorPane localPane;
+
+    @FXML
+    private Button scanButton;
+
+    @FXML
+    private AnchorPane connectedPane;
+    
+    
+    public void LocalDevicesPage() {
+		
+	}
+    
+    public void ConnectedPage() {
+		
+	}
+
+}
