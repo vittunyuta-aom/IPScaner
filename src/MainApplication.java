ce962bde33c1686bd32125ebd4dc6b7125a836b8
Septima777 <noppawan.kul@ku.th>
2018-11-26 16:16:51 +0700

    modified localDevicesPage and add timeChatPage

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -13,6 +13,7 @@ public class MainApplication extends Application{
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 			
 		} catch (Exception e) {
--- src/ScannerController.java
+++ src/ScannerController.java
@@ -1,30 +1,71 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
-public class ScannerController {
+public class ScannerController implements Initializable{
 
 	@FXML
     private Button localDeviceButton;
 
     @FXML
-    private Button ConnectedButton;
+    private Button timeButton;
 
     @FXML
     private AnchorPane localPane;
 
     @FXML
-    private Button scanButton;
+    private Button startButton;
 
     @FXML
-    private AnchorPane connectedPane;
+    private Button stopButton;
+
+    @FXML
+    private AnchorPane timePane;
+    
+    @FXML
+    private Text timeStart;
+
+    @FXML
+    private Text timeStop;
+
     
     
-    public void LocalDevicesPage() {
-		
+    public void LocalDevicesPage(ActionEvent event) {
+		localPane.setVisible(true);
+		timePane.setVisible(false);
 	}
     
-    public void ConnectedPage() {
+    public void TimePage(ActionEvent event) {
+    	localPane.setVisible(false);
+		timePane.setVisible(true);
+	}
+    
+    public void Start(ActionEvent event){
+    	timeStart.setText("12.22");
+    	
+    }
+    
+    public void Stop(ActionEvent event){
+    	timeStop.setText("14.30");
+    }
+    
+    public void Reset(ActionEvent event){
+    	timeStart.setText("Time Start");
+    	timeStop.setText("Time Stop");
+    	
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 		
 	}
 
