08024f33040f1f53c0f29a89261c8123204f00b0
aommoaGitHub <vittunyuta@gmail.com>
2018-11-26 15:48:19 +0700

    added auto repeat scan

--- src/scanners/Scanner.java
+++ src/scanners/Scanner.java
@@ -1,19 +1,49 @@
+package scanners;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
-public class Scanner {
+public class Scanner extends TimerTask {
     private static List<String> scannedTime = new ArrayList<String>();
     private static List<ScannedDevice> reachableAddresses = new ArrayList<ScannedDevice>();
     private final DateFormat TIMEFORMAT = new SimpleDateFormat("HH:mm:ss");
+    private Timer timer = new Timer();
+    private boolean isScanning = false;
+    private static Scanner scanner = new Scanner();
 
+    private Scanner(){}
 
-    public List<ScannedDevice> startScan() {
+    public static Scanner instance(){
+        return scanner;
+    }
+
+    public void startScan(){
+        System.out.println(isScanning);
+        if(!isScanning){
+            isScanning = true;
+            timer.schedule(this, 0,5000);
+        }
+        else
+            System.err.println("scanners.Scanner is scanning");
+    }
+
+    public void stopScan(){
+        isScanning = false;
+        timer.cancel();
+        timer.purge();
+    }
+
+    @Override
+    public void run() {
+        scan();
+        System.out.println("-------------------------------");
+    }
+
+    private List<ScannedDevice> scan() {
         Date currentTimeScan = new Date();
         scannedTime.add(TIMEFORMAT.format(currentTimeScan));
 
@@ -78,4 +108,7 @@ public class Scanner {
         return -1;
     }
 
+    public static List<ScannedDevice> getReachableAddresses() {
+        return reachableAddresses;
+    }
 }
